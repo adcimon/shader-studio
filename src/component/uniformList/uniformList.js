cb90dd7512c7749367085bad6d532c0831a94a4f
adcimon <37084114+adcimon@users.noreply.github.com>
2021-12-17 22:41:01 +0100

    Minor improvements

---
+++ src/component/uniformList/uniformList.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const css = `<link type="text/css" rel="stylesheet" href="./src/component/uniformList/style.css">`;
+
+const html = `
+<div>
+</div>
+`;
+
+export class UniformList extends HTMLElement
+{
+    constructor()
+    {
+        super();
+
+        const template = document.createElement("template");
+        template.innerHTML = css + html;
+
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback()
+    {
+    }
+
+    disconnectedCallback()
+    {
+    }
+}
+
+window.customElements.define("uniform-list", UniformList);
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 "use strict";
 
-import { Fragments } from './render/fragments.js';
-import { Quad } from './render/quad.js';
-import { Texture } from './render/texture.js';
-import { Shader } from './render/shader.js';
 import { Renderer } from './render/renderer.js';
+import { Shader } from './render/shader.js';
+import { Texture } from './render/texture.js';
+import { Quad } from './render/quad.js';
+import { Fragments } from './render/fragments.js';
 
-import { CodeEditor } from './component/codeEditor/codeEditor.js';
-import { NavigationMenu } from './component/navigationMenu/navigationMenu.js';
-import { EditorView } from './component/editorView/editorView.js';
-import { RenderView } from './component/renderView/renderView.js';
 import { MainView } from './component/mainView/mainView.js';
+import { RenderView } from './component/renderView/renderView.js';
+import { EditorView } from './component/editorView/editorView.js';
+import { NavigationMenu } from './component/navigationMenu/navigationMenu.js';
+import { CodeEditor } from './component/codeEditor/codeEditor.js';
+import { UniformList } from './component/uniformList/uniformList.js';
 
-var codeEditor, renderView;
-var gl, quad, shader, renderer;
+var renderView, navigationMenu, codeEditor, uniformList;
+var gl, renderer, shader, quad;
 
 window.addEventListener("load", main);
 window.addEventListener("beforeunload", exit);
@@ -30,8 +31,12 @@ function exit()
 
 function initializeInterface()
 {
+    navigationMenu = document.body.querySelector("navigation-menu");
+
     codeEditor = document.body.querySelector("code-editor");
     codeEditor.setCode(Fragments[0]);
+
+    uniformList = new UniformList();
 
     renderView = document.body.querySelector("render-view");
     renderView.resize();
