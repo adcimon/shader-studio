ea45ffdaeb4817043ae4fcc60a532cf279e53a4c
Adri <Adri.Ciborro.Montes@gmail.com>
2022-01-08 12:57:08 +0100

    Updated icons

---
+++ src/component/navigationMenu/icons.js
@@ -0,0 +1,277 @@
+"use strict";
+
+export const ShaderIcon =
+`
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" id="svg2" xml:space="preserve" width="85.333336" height="85.333336" viewBox="0 0 85.333336 85.333336" sodipodi:docname="shader.ai">
+  <metadata id="metadata8">
+    <rdf:RDF>
+      <cc:Work rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <defs id="defs6">
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath18">
+      <path d="M 0,0 H 64 V 64 H 0 Z" id="path16" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath30">
+      <path d="M 10.6836,52 H 54.0449 V 7.98633 H 10.6836 Z" id="path28" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath34">
+      <path d="M 10.684,7.987 H 54.045 V 52 H 10.684 Z" id="path32" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath58">
+      <path d="m 16.7153,43 h 8 v -8 h -8 z" id="path56" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath62">
+      <path d="m 16.715,35 h 8 v 8 h -8 z" id="path60" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath82">
+      <path d="m 28,48.3872 h 8 v -8 h -8 z" id="path80" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath86">
+      <path d="m 28,40.387 h 8 v 8 h -8 z" id="path84" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath106">
+      <path d="m 39.2109,43.272 h 8 v -8 h -8 z" id="path104" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath110">
+      <path d="m 39.211,35.272 h 8 v 8 h -8 z" id="path108" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath130">
+      <path d="m 42.1875,30.7295 h 8 v -8 h -8 z" id="path128" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath134">
+      <path d="m 42.188,22.729 h 8 v 8 h -8 z" id="path132" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath154">
+      <path d="m 33.1045,20.8955 h 8 v -8 h -8 z" id="path152" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath158">
+      <path d="m 33.104,12.896 h 8 v 8 h -8 z" id="path156" />
+    </clipPath>
+  </defs>
+  <sodipodi:namedview pagecolor="#ffffff" bordercolor="#666666" borderopacity="1" objecttolerance="10" gridtolerance="10" guidetolerance="10" inkscape:pageopacity="0" inkscape:pageshadow="2" inkscape:window-width="640" inkscape:window-height="480" id="namedview4" />
+  <g id="g10" inkscape:groupmode="layer" inkscape:label="shader" transform="matrix(1.3333333,0,0,-1.3333333,0,85.333333)">
+    <g id="g12">
+      <g id="g14" clip-path="url(#clipPath18)">
+        <g id="g20" transform="translate(63.833,32)">
+          <path d="m 0,0 c 0,-17.673 -14.327,-32 -32,-32 -17.673,0 -32,14.327 -32,32 0,17.673 14.327,32 32,32 C -14.327,32 0,17.673 0,0" style="fill:transparent;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path22" />
+        </g>
+        <g id="g24">
+          <g id="g26" />
+          <g id="g46">
+            <g clip-path="url(#clipPath30)" opacity="0.199997" id="g44">
+              <g id="g42">
+                <g clip-path="url(#clipPath34)" id="g40">
+                  <g transform="translate(24,16)" id="g38">
+                    <path d="M 0,0 C -2.209,0 -4,1.791 -4,4 -4,6.209 -2.209,8 0,8 2.209,8 4,6.209 4,4 4,1.791 2.209,0 0,0 m 8,36 c -8.545,0 -18,-5.375 -21.083,-15.656 -1.558,-5.194 5.187,-3.118 5.646,-9.156 0.375,-4.938 -1.114,-6.6 -1.438,-9.563 -0.438,-4 8.875,-10 17.24,-9.621 C 20.64,-7.439 30.045,1.712 30.045,14 30.045,26.288 20.288,36 8,36" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path36" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g48" transform="translate(24,18)">
+          <path d="M 0,0 C -2.209,0 -4,1.791 -4,4 -4,6.209 -2.209,8 0,8 2.209,8 4,6.209 4,4 4,1.791 2.209,0 0,0 m 8,36 c -8.545,0 -18,-5.375 -21.083,-15.656 -1.558,-5.194 5.187,-3.118 5.646,-9.156 0.375,-4.938 -1.114,-6.6 -1.438,-9.563 -0.438,-4 8.875,-10 17.24,-9.621 C 20.64,-7.439 30.045,1.712 30.045,14 30.045,26.288 20.288,36 8,36" style="fill:#e0995e;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path50" />
+        </g>
+        <g id="g52">
+          <g id="g54" />
+          <g id="g74">
+            <g clip-path="url(#clipPath58)" opacity="0.199997" id="g72">
+              <g id="g70">
+                <g clip-path="url(#clipPath62)" id="g68">
+                  <g transform="translate(24.7153,39)" id="g66">
+                    <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path64" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g76">
+          <g id="g78" />
+          <g id="g98">
+            <g clip-path="url(#clipPath82)" opacity="0.199997" id="g96">
+              <g id="g94">
+                <g clip-path="url(#clipPath86)" id="g92">
+                  <g transform="translate(36,44.3872)" id="g90">
+                    <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path88" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g100">
+          <g id="g102" />
+          <g id="g122">
+            <g clip-path="url(#clipPath106)" opacity="0.199997" id="g120">
+              <g id="g118">
+                <g clip-path="url(#clipPath110)" id="g116">
+                  <g transform="translate(47.2109,39.272)" id="g114">
+                    <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path112" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g124">
+          <g id="g126" />
+          <g id="g146">
+            <g clip-path="url(#clipPath130)" opacity="0.199997" id="g144">
+              <g id="g142">
+                <g clip-path="url(#clipPath134)" id="g140">
+                  <g transform="translate(50.1875,26.7295)" id="g138">
+                    <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path136" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g148">
+          <g id="g150" />
+          <g id="g170">
+            <g clip-path="url(#clipPath154)" opacity="0.199997" id="g168">
+              <g id="g166">
+                <g clip-path="url(#clipPath158)" id="g164">
+                  <g transform="translate(41.1045,16.8955)" id="g162">
+                    <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path160" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g172" transform="translate(24.7153,41)">
+          <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#c75c5c;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path174" />
+        </g>
+        <g id="g176" transform="translate(36,46.3872)">
+          <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#f5cf87;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path178" />
+        </g>
+        <g id="g180" transform="translate(47.2109,41.272)">
+          <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#76c2af;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path182" />
+        </g>
+        <g id="g184" transform="translate(50.1875,28.7295)">
+          <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path186" />
+        </g>
+        <g id="g188" transform="translate(41.1045,18.8955)">
+          <path d="m 0,0 c 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4 2.209,0 4,-1.791 4,-4" style="fill:#4f5d73;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path190" />
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
+`;
+
+export const UniformsIcon =
+`
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" id="svg2" xml:space="preserve" width="85.333336" height="85.333336" viewBox="0 0 85.333336 85.333336" sodipodi:docname="uniforms.ai">
+  <metadata id="metadata8">
+    <rdf:RDF>
+      <cc:Work rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <defs id="defs6">
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath18">
+      <path d="M 0,0 H 64 V 64 H 0 Z" id="path16" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath42">
+      <path d="m 34,49.8643 h 8 v -8 h -8 z" id="path40" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath46">
+      <path d="m 42,49.864 h -8 v -8 h 8 z" id="path44" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath66">
+      <path d="m 23,36 h 8 v -8 h -8 z" id="path64" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath70">
+      <path d="m 31,36 h -8 v -8 h 8 z" id="path68" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath90">
+      <path d="m 40,22 h 8 v -8 h -8 z" id="path88" />
+    </clipPath>
+    <clipPath clipPathUnits="userSpaceOnUse" id="clipPath94">
+      <path d="m 48,22 h -8 v -8 h 8 z" id="path92" />
+    </clipPath>
+  </defs>
+  <sodipodi:namedview pagecolor="#ffffff" bordercolor="#666666" borderopacity="1" objecttolerance="10" gridtolerance="10" guidetolerance="10" inkscape:pageopacity="0" inkscape:pageshadow="2" inkscape:window-width="640" inkscape:window-height="480" id="namedview4" />
+  <g id="g10" inkscape:groupmode="layer" inkscape:label="uniforms" transform="matrix(1.3333333,0,0,-1.3333333,0,85.333333)">
+    <g id="g12">
+      <g id="g14" clip-path="url(#clipPath18)">
+        <g id="g20" transform="translate(32,64)">
+          <path d="m 0,0 c 17.673,0 32,-14.327 32,-32 0,-17.673 -14.327,-32 -32,-32 -17.673,0 -32,14.327 -32,32 0,17.673 14.327,32 32,32" style="fill:transparent;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path22" />
+        </g>
+        <g id="g24" transform="translate(52,32)">
+          <path d="m 0,0 c 0,-1.105 -0.895,-2 -2,-2 h -32 c -1.105,0 -2,0.895 -2,2 0,1.105 0.895,2 2,2 H -2 C -0.895,2 0,1.105 0,0" style="fill:#4f5d73;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path26" />
+        </g>
+        <g id="g28" transform="translate(52,46)">
+          <path d="m 0,0 c 0,-1.105 -0.895,-2 -2,-2 h -32 c -1.105,0 -2,0.895 -2,2 0,1.105 0.895,2 2,2 H -2 C -0.895,2 0,1.105 0,0" style="fill:#4f5d73;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path30" />
+        </g>
+        <g id="g32" transform="translate(52,18)">
+          <path d="m 0,0 c 0,-1.105 -0.895,-2 -2,-2 h -32 c -1.105,0 -2,0.895 -2,2 0,1.105 0.895,2 2,2 H -2 C -0.895,2 0,1.105 0,0" style="fill:#4f5d73;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path34" />
+        </g>
+        <g id="g36">
+          <g id="g38" />
+          <g id="g58">
+            <g clip-path="url(#clipPath42)" opacity="0.199997" id="g56">
+              <g id="g54">
+                <g clip-path="url(#clipPath46)" id="g52">
+                  <g transform="translate(38,49.8643)" id="g50">
+                    <path d="m 0,0 c 2.209,0 4,-1.791 4,-4 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path48" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g60">
+          <g id="g62" />
+          <g id="g82">
+            <g clip-path="url(#clipPath66)" opacity="0.199997" id="g80">
+              <g id="g78">
+                <g clip-path="url(#clipPath70)" id="g76">
+                  <g transform="translate(27,36)" id="g74">
+                    <path d="m 0,0 c 2.209,0 4,-1.791 4,-4 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path72" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g84">
+          <g id="g86" />
+          <g id="g106">
+            <g clip-path="url(#clipPath90)" opacity="0.199997" id="g104">
+              <g id="g102">
+                <g clip-path="url(#clipPath94)" id="g100">
+                  <g transform="translate(44,22)" id="g98">
+                    <path d="m 0,0 c 2.209,0 4,-1.791 4,-4 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4" style="fill:#231f20;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path96" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="g108" transform="translate(36,49.8643)">
+          <path d="m 0,0 c 2.209,0 4,-1.791 4,-4 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4" style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path110" />
+        </g>
+        <g id="g112" transform="translate(25,36)">
+          <path d="m 0,0 c 2.209,0 4,-1.791 4,-4 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4" style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path114" />
+        </g>
+        <g id="g116" transform="translate(42,22)">
+          <path d="m 0,0 c 2.209,0 4,-1.791 4,-4 0,-2.209 -1.791,-4 -4,-4 -2.209,0 -4,1.791 -4,4 0,2.209 1.791,4 4,4" style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path118" />
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
+`;
