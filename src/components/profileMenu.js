5ed6051196c04159ea6604115e3fbc27e02aa1e3
adcimon <37084114+adcimon@users.noreply.github.com>
2022-10-26 14:15:04 +0200

    Minor change

--- src/components/profileMenu.js
+++ src/components/profileMenu.js
@@ -39,7 +39,6 @@ const html = /*html*/
                 <a
                     class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                     href="#"
-                    aria-label="Toggle Theme"
                     x-on:click="$store.app.toggleTheme()">
                     <svg
                         class="w-5 h-5 mr-2"
